17eb1d23076b70dcef0cc111a8bf1be3f8275698
Vladimir Jimenez <allejo@me.com>
2023-08-17 12:10:37 -0700

    Add more links to the footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,16 +1,44 @@
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faHeart } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+const links = [
+	{ href: 'https://allejo.io', text: '@allejo' },
+	{
+		href: 'https://github.com/allejo/json-reshaper',
+		text: 'GitHub',
+		icon: faGithub,
+	},
+	{
+		href: 'https://github.com/sponsors/allejo',
+		text: 'Sponsor',
+		icon: faHeart,
+		iconClass: 'text-red-500',
+	},
+];
+
 export const Footer = () => (
 	<footer className="grid grid-cols-2">
 		<div>
 			<span className="text-slate-500">
 				Copyright &copy; {new Date().getFullYear()}
 			</span>
-			<a
-				href="https://allejo.io"
-				target="_blank"
-				className="inline-block ml-4 font-bold text-slate-800 underline cursor-pointer"
-			>
-				@allejo
-			</a>
+			{links.map(({ href, text, icon, iconClass }, index) => (
+				<a
+					key={index}
+					href={href}
+					target="_blank"
+					className="inline-block ml-4 font-bold text-slate-800 underline cursor-pointer"
+				>
+					{icon && (
+						<FontAwesomeIcon
+							icon={icon}
+							className={`${iconClass ?? ''} mr-1`}
+						/>
+					)}
+					{text}
+				</a>
+			))}
 		</div>
 	</footer>
 );
