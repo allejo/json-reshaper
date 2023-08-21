2bfa1f6324399d29f79ef044f1e35973b07ee779
Vladimir Jimenez <allejo@me.com>
2023-08-20 19:27:25 -0700

    Run prettier on the codebase again

--- src/components/OutputPreview.tsx
+++ src/components/OutputPreview.tsx
@@ -21,8 +21,8 @@ interface Props {
 }
 const MimeType: Record<OutputFormat, string> = {
 	[OutputFormat.CSV]: 'text/csv',
-	[OutputFormat.TSV]: 'text/tab-seprated-values' 
-}
+	[OutputFormat.TSV]: 'text/tab-seprated-values',
+};
 
 export const OutputPreview = ({ columnQueries, filteredJson }: Props) => {
 	const [format, setFormat] = useState<OutputFormat>(OutputFormat.CSV);
@@ -56,19 +56,19 @@ export const OutputPreview = ({ columnQueries, filteredJson }: Props) => {
 			});
 	}, [copy]);
 
-	const handleDownload = useCallback(()=>{		
+	const handleDownload = useCallback(() => {
 		const transformedText = getTransformedAsText.current();
 		const type = MimeType[format];
-		const data = new Blob([transformedText],{type});
+		const data = new Blob([transformedText], { type });
 		const url = window.URL.createObjectURL(data);
 		setFileLink(url);
-	}, [format])
+	}, [format]);
 
 	return (
 		<section className="flex flex-col min-w-0">
 			<p className="font-bold mb-2">Output Preview</p>
 			<div className="bg-white border flex rounded mb-4 p-3">
-				<div className='flex items-center'>
+				<div className="flex items-center">
 					<label className="inline-block font-bold mr-3 my-0" htmlFor="format">
 						Format
 					</label>
@@ -88,7 +88,12 @@ export const OutputPreview = ({ columnQueries, filteredJson }: Props) => {
 					<button className="border py-1 px-2" onClick={handleCopyEvent}>
 						Copy as {format.toUpperCase()}
 					</button>
-					<a download={`json-reshaper.${format}`} href={fileLink} className="border py-1 px-2" onClick={handleDownload}>
+					<a
+						download={`json-reshaper.${format}`}
+						href={fileLink}
+						className="border py-1 px-2"
+						onClick={handleDownload}
+					>
 						Download {format.toUpperCase()}
 					</a>
 				</div>
